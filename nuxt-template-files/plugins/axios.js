ede8a2ae850f101011433f4a25df0e796aa4a223
Jon Kristensen <jon@nejla.com>
2021-06-01 18:36:33 +0200

    Add incremental update

--- nuxt-template-files/plugins/axios.js
+++ nuxt-template-files/plugins/axios.js
@@ -1,6 +1,6 @@
 export default function({ $axios, redirect, store }) {
   $axios.onResponseError((error) => {
-    if (error.response && error.reponse.status === 403) {
+    if (error.status === 403) {
       if (process.server) {
         redirect('/sign-in')
       } else {
